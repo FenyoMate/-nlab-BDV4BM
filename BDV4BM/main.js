54a1544e029800d5866297fa60c5bc3f156269d2
FenyoMate <fenyo.mate.steam9913@gmail.com>
2022-03-27 18:18:34 +0200

    asd

--- BDV4BM/main.js
+++ BDV4BM/main.js
@@ -1,2 +1,13 @@
-import cytoscape from "./cytoscape.js-3.21.0/src"; 
-var cy = cytoscape({ elements: myElements, container: myDiv });
+import cytoscape from "D:\Egyetem\BDV4BM\-nlab-BDV4BM\BDV4BM\cytoscape.js-3.21.0\src\index.js"; 
+
+//Initialization
+cytoscape.warnings(true);
+
+
+var cy = cytoscape({
+    container: document.getElementById('cy')
+  });
+console.log("Elements loaded, structure initialized");
+cytoscape.warnings();
+ele.cy();
+
